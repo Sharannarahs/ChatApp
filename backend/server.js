aa6d96a9af333a9602b830e7c5d716f856f16fd7
Sharannarahs <sharan832004@gmail.com>
2025-07-21 23:13:04 +0530

    final commit

--- backend/server.js
+++ backend/server.js
@@ -67,4 +67,10 @@ await connectDB();
 
 const PORT = process.env.PORT || 5000;
 
-server.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`));
+if(process.env.NODE_ENV !== "production"){
+    server.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`));
+
+}
+
+// EXPORT SERVER FOR VERCEL
+export default server;
